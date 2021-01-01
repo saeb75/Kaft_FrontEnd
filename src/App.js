6e559071d59e1e5e50a025a40553a32b02f51f43
saeb jafari <saebfat@gmail.com>
2021-01-01 22:41:45 +0330

    add project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Switch from "react-bootstrap/esm/Switch";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import ProductList from "./Pages/ProductsList/ProductList";
+import Product from "./Pages/Product/Product";
+import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch style={{ padding: 0 }}>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" component={SignUp} />
+        <Route path="/product" component={Product} />
+        <Route path="/category" component={ProductList} />
+      </Switch>
+    </>
   );
 }
 
